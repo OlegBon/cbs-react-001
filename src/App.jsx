f1656486054083987934a75c8ada5a0e46fd7c5d
OlegBon <oleg_bon@ukr.net>
2024-10-17 12:18:06 +0300

    Import img

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import myImgImport from '/img-01.jpg'
 import './App.css'
 
 function App() {
@@ -9,6 +10,8 @@ function App() {
   return (
     <>
       <div>
+        <img src={myImgImport} className="myImgImport" alt="myImgImport" />
+        <br />
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
